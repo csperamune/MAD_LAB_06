730e4581aac4e76fe100175ef0d7fa997862d467
Chamith <csperamune@gmail.com>
2020-05-25 18:22:51 +0530

    Recycler View

---
+++ app/src/main/java/com/example/mad_lab_06/layout_list.java
@@ -0,0 +1,14 @@
+package com.example.mad_lab_06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class layout_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_list);
+    }
+}
